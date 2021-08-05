c07cad99ba15fbfbdcccf42b5b551e2464c4213a
Matt <mattlacaire@gmail.com>
2021-08-05 13:45:48 -0700

    Cleaned up code and added local time output.

--- Mid-Range Code/WitMotionCode.py
+++ Mid-Range Code/WitMotionCode.py
@@ -5,15 +5,15 @@
 #% RM3100 code. 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-import os
 import serial
+import datetime
 import time
 address = '/dev/ttyUSB0'
 BAUDRATE = 9600
-numChars = 11 #I don't think this can change. This is how many characters are in a message to be parsed. 
+numChars = 11 #Length in bytes of each message. 
 
 def openSerial():
-    ser = serial.Serial(address, BAUDRATE, bytesize = 8) #this should be correct. 
+    ser = serial.Serial(address, BAUDRATE, bytesize = 8) 
     return ser
 
 def ParseData(dataarray):
@@ -22,27 +22,24 @@ def ParseData(dataarray):
         Hy = ((dataarray[5] << 8) | dataarray[6]) 
         Hz = ((dataarray[7] << 8) | dataarray[8])
         print(f"x: {Hx}, y: {Hy}, z: {Hz}")
-    else:
-        print("Other data type")
+        time = datetime.datetime.now()
+        print(time)
 
 
-
-
-port = openSerial()
-
-if (port):
+try:
+    port = openSerial()
     print(f"Successfully opened port at {address}")
-else:
+except serial.SerialException:
     print("Port Opening Failure") #deal with this later
-    
+    time.sleep(1)
+    exit() 
+
+
 while(port.is_open):
     x = port.read(numChars)
-    ParseData(x)
-
-    
-
-       
-    
-    
-    
-    
+    if x[0] == 85:
+        ParseData(x)
+    else:
+        print("Bytestring not properly synced. Trying again...")
+        port.close()
+        port = openSerial()
