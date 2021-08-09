08c4766ac307a2a6f7c16545592d63c7ee78299d
Matt <mattlacaire@gmail.com>
2021-08-09 13:25:44 -0700

    Still uncalibrated for magnitude. Negative field outputs now possible.

--- Mid-Range Code/WitMotionCode.py
+++ Mid-Range Code/WitMotionCode.py
@@ -16,14 +16,28 @@ def openSerial():
     ser = serial.Serial(address, BAUDRATE, bytesize = 8) 
     return ser
 
+def twos(val_str, bytes): #performs two's complement operation. This is roundabout, but the x object is returned in decimal for some reason.
+    import sys
+    b = val_str.to_bytes(bytes, byteorder=sys.byteorder, signed=False)
+    return int.from_bytes(b, byteorder=sys.byteorder, signed=True)
+
+
 def ParseData(dataarray):
     if dataarray[0] == 85 and dataarray[1] == 84:
         Hx = ((dataarray[3] << 8) | dataarray[4]) 
         Hy = ((dataarray[5] << 8) | dataarray[6]) 
         Hz = ((dataarray[7] << 8) | dataarray[8])
-        print(f"x: {Hx}, y: {Hy}, z: {Hz}")
-        time = datetime.datetime.now()
-        print(time)
+        if Hx <= 2**15 and Hy <= 2**15 and Hz <= 2**15: #half of a sixteen-bit value's range; this condition being met means all fields are positive.
+            print(f"x: {Hx}, y: {Hy}, z: {Hz}")
+            time = datetime.datetime.now()
+            print(time)
+        else:
+            Hx = twos(Hx, 2)
+            Hy = twos(Hy, 2)
+            Hz = twos(Hz, 2)
+            print(f"x: {Hx}, y: {Hy}, z: {Hz}")
+            time = datetime.datetime.now()
+            print(time)
 
 
 try:
