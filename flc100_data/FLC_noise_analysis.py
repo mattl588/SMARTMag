71628fb5f23d91e24ea61e045c9c6869c109f5fb
Matt Lacaire <71798926+mattl588@users.noreply.github.com>
2022-01-28 00:05:26 -0800

    Add files via upload

---
+++ flc100_data/FLC_noise_analysis.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 21 14:22:06 2021
+
+@author: Matt
+"""
+
+import numpy as np
+import os 
+import time
+
+with open('log.txt','r') as file:
+    for i in file:
+        print(file)
+        
